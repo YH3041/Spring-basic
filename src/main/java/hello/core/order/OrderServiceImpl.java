f49469261e2c9564d77fbe9f5ae30e53c2c99b15
YH3041 <mmj2rv9@gmail.com>
2021-01-13 13:38:27 +0900

    ReteDiscountPolicy 개발

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -11,7 +12,9 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private  final  DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    private  DiscountPolicy discountPolicy; //인터페이스에만 의존하지만 NPE 발생
 
 
     @Override
